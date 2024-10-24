24eb4bf5a585fd42a274e01d4434f9b3033beef1
Cody Taylor <codemister99@yahoo.com>
2024-10-24 19:08:20 -0400

    06 Pest Driving Posts

---
+++ resources/js/Components/Container.vue
@@ -0,0 +1,4 @@
+<template lang="pug">
+    .max-w-7xl.mx-auto.py-10(class="sm:px-6 lg:px-8")
+        slot
+</template>
---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Container from '@/Components/Container.vue';
+import AppLayout from '@/Layouts/AppLayout.vue'
+
+defineProps(['posts']);
+</script>
+
+<template lang="pug">
+    AppLayout
+        Container
+            ul.divide-y
+                li.px-2.py-4(v-for="post in posts" :key="post.id")
+                    span.font-bold.text-lg {{ post.title }}
+</template>
