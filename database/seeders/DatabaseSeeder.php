9fc664bfb6a915ebc87a772753b30cba52834bff
Cody Taylor <codemister99@yahoo.com>
2024-10-24 16:56:20 -0400

    04 Factories and Seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = User::factory(10)->create();
+        $posts = Post::factory(200)->recycle($users)->create();
+        $comments = Comment::factory(100)->recycle($users)->recycle($posts)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $dev_bro = User::factory()
+            ->has(Post::factory(25))
+            ->has(Comment::factory(50)->recycle($posts))
+            ->create([
+                'name' => 'Dev Bro',
+                'email' => 'dev.bro@hostdoc.forum',
+            ]);
     }
 }
